4fc7a1bc3191c61377512fa3645ec864aa381f6f
Ujjwal Suman <ujjwalsuman007@gmail.com>
2023-12-13 16:19:51 +0530

    commit 2
    cloudinary  config changes

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -4,6 +4,9 @@ const cloudinary = require('cloudinary').v2;
 exports.cloudinaryConnect = () =>{
     try{
         cloudinary.config({
+            // cloud_name:process.env.CLOUD_NAME,
+            // api_key: process.env.API_KEY,
+            // api_secret: process.env.API_SECRET,
             cloud_name:'dagnvnglm',
             api_key:'981646744474671',
             api_secret:'d8zo3orbjenLG-3vpnIsOV_Nnic',
